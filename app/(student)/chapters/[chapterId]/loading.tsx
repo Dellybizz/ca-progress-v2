83f889a28fb00ab846aa88446a13a52cf624dc95
Dellybizz <asmazuberi7@gmail.com>
2026-09-06 15:38:13 +0530

    feat(chapter-hub): add route loading state

---
+++ app/(student)/chapters/[chapterId]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return <div className="chapter-hub-page" aria-label="Loading Chapter Hub">
+    <div className="chapter-hub-loading">
+      <span/><span/><span/><span/>
+    </div>
+  </div>;
+}
