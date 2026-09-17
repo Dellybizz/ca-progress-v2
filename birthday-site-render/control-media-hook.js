cfb05bee3fb28f717e09e9da9fbc3cae82253a3b
Dellybizz <asmazuberi7@gmail.com>
2026-09-18 03:09:14 +0530

    Bust editor cache for media and text fixes

--- birthday-site-render/control-media-hook.js
+++ birthday-site-render/control-media-hook.js
@@ -2,7 +2,7 @@
   const previousWrite=document.write.bind(document);
   document.write=function(html){
     if(typeof html==='string'&&html.includes('Birthday Site Control Room')){
-      const script='<script src="./control-media-runtime.js?v=7"></'+'script>';
+      const script='<script src="./control-media-runtime.js?v=8"></'+'script>';
       const i=html.toLowerCase().lastIndexOf('</body>');
       html=i>=0?html.slice(0,i)+script+html.slice(i):html+script;
     }
