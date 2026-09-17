fb6f483162372289669a0b78dad1582acfdca1b2
Dellybizz <asmazuberi7@gmail.com>
2026-09-18 03:30:50 +0530

    Bust page patch runtime cache

--- birthday-site-render/page-runtime-hook.js
+++ birthday-site-render/page-runtime-hook.js
@@ -2,7 +2,7 @@
   const previousWrite=document.write.bind(document);
   document.write=function(html){
     if(typeof html==='string'&&!html.includes('Birthday Site Control Room')){
-      const script='<script src="./page-patch-runtime.js"></'+'script>';
+      const script='<script src="./page-patch-runtime.js?v=12"></'+'script>';
       const i=html.toLowerCase().lastIndexOf('</body>');
       html=i>=0?html.slice(0,i)+script+html.slice(i):html+script;
     }
